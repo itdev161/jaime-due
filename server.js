9e8a83e27cd6879da048f633ac7cb9ac95517b05
jaimedue <braxtonkeil@Braxtons-MacBook-Pro.local>
2020-09-30 16:27:29 -0500

    commit

--- server.js
+++ server.js
@@ -7,10 +7,27 @@ const app = express();
 // Connect database
 connectDatabase();
 
+// Configure Middleare
+app.use(express.json({ extended: false }));
+
 // API endpoints
+/**
+ *  @route GET /
+ *  @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ *  @route Post api/users
+ *  @desc Register user
+ */
+
+ app.post('/api/users', (req, res) => {
+     console.log(req.body);
+     res.send(req.body);
+ });
 
 // Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
