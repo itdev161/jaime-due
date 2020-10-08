84ec0ff49efd1ad7b2bd1d0a12ea41ee27b113ce
jaimedue <braxtonkeil@Braxtons-MacBook-Pro.local>
2020-10-07 20:20:05 -0500

    commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+         GoodThings
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
