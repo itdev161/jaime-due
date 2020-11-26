debf330b8c5c66ddafbdab4605572d476e866934
jaimedue <braxtonkeil@Braxtons-MacBook-Pro.local>
2020-11-25 18:42:00 -0600

    PostList and Post Components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
-/*import { render } from '@testing-library/react';*/
 import React from 'react';
 import './App.css';
 import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import PostList from './components/PostList/PostList';
+import Post from './components/Post/Post';
 
 class App extends React.Component {
   state = {
     posts: [],
+    post: null,
     token: null,
     user: null
-  }
+  };
 
   componentDidMount() {
       this.authenticateUser();
@@ -80,8 +82,15 @@ class App extends React.Component {
     this.setState({ user: null, token: null });
   }
 
+  viewPost = post => {
+    console.log(`view ${post.title}`);
+    this.setState({
+      post: post
+    });
+  };
+
   render() {
-    let { user, posts } = this.state;
+    let { user, posts, post } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
     };
@@ -108,30 +117,30 @@ class App extends React.Component {
           </ul>
           </header>
           <main>
-            <Route exact path="/">
-              {user ? (
-                <React.Fragment>
-                  <div>Hello {user}!</div>
-                  <div>
-                    {posts.map(post => (
-                      <div key={post._id}>
-                        <h1>{post.title}</h1>
-                        <p>{post.body}</p>
-                      </div>
-                    ))}
-                  </div>
-                </React.Fragment> 
-              ) : (
-                <React.Fragment>Please Register or Login</React.Fragment>
-              )}
-            </Route>
             <Switch>
+              <Route exact path="/">
+                {user ? (
+                  <React.Fragment>
+                    <div>Hello {user}!</div>
+                    <PostList posts={posts} clickPost={this.viewPost} />
+                  </React.Fragment> 
+                ) : (
+                  <React.Fragment>Please Register or Login</React.Fragment>
+                )}
+              </Route>
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
               <Route
-                exact path="/register"
-                render={() => <Register {...authProps} />} />
+                exact
+                path="/register"
+                render={() => <Register {...authProps} />}
+              />
               <Route
-                exact path="/login"
-                render={() => <Login {...authProps} />} />
+                exact
+                path="/login"
+                render={() => <Login {...authProps} />}
+              />
             </Switch>
           </main>
         </div>
